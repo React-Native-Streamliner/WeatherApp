523da45c72c8f46c9891f001e5e8c0db1f8aa7e5
tiger <thisisstreamliner@gmail.com>
2023-05-14 15:31:18 +0300

    Styling Basics

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React from "react"
-import { View, Text, SafeAreaView } from "react-native"
+import { View, Text, SafeAreaView, StyleSheet } from "react-native"
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
         <Text>Current Weather</Text>
       </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1
+  },
+  container: {
+    backgroundColor: 'pink',
+    flex: 1,
+    alignItems: 'center'
+  }
+})
 
 export default App
