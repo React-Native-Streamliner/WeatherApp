9a87cb07812a9983a9f722579947c73d100002a4
tiger <thisisstreamliner@gmail.com>
2023-05-26 21:43:53 +0300

    Add MockData for UpcomingWeather

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,9 +1,52 @@
-import { SafeAreaView, StyleSheet, Text } from "react-native"
+import { SafeAreaView, StyleSheet, Text, FlatList } from "react-native"
+
+const DATA = [
+    {
+        dt_txt: "2023-02-18 12:00:00",
+        main: {
+            temp_max: 8.55,
+            temp_min: 7.55
+        },
+        weather: [
+            {
+                main: 'Clear'
+            }
+        ]
+    },
+    {
+        dt_txt: "2023-02-18 15:00:00",
+        main: {
+            temp_max: 8.55,
+            temp_min: 7.55
+        },
+        weather: [
+            {
+                main: 'Clouds'
+            }
+        ]
+    },
+    {
+        dt_txt: "2023-02-18 12:00:00",
+        main: {
+            temp_max: 8.55,
+            temp_min: 7.55
+        },
+        weather: [
+            {
+                main: 'Rain'
+            }
+        ]
+    }
+]
 
 const UpcomingWeather = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Text>Upcoming Weather</Text>
+            <FlatList 
+                data={}
+                renderItem={}
+            />
         </SafeAreaView>
     )
 }
